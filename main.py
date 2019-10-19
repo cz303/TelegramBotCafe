2de98657332fd04c1fab7ae9f2556e2c22c707cc
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-19 03:01:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import telebot
 
 bot = telebot.TeleBot("951388162:AAEtCaLOXZMXrepJ4S0r09bKHNzdZQNxyuE")
 
-print(bot.get_me())
-
-def log(message,answer):
-    print("/n ------")
-    from datetime import datetime
-    print(datetime.now())
-    print("Сообщение от {0} {1}. (id = {2}) \n Текст - {3}".format(message.from_user.first_name,
-                                                                   message.from_user.last_name,
-                                                                   str(message.from_user.id),
-                                                                   message.text))
-    print(answer)
-
-
 @bot.message_handler(commands=['start'])
 def handle_text(message):
     user_markup = telebot.types.ReplyKeyboardMarkup()
